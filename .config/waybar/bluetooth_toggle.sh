709022726700a8b358e2b5a0dadf53896acc1dbe
Bob Varioa <true.email001@gmail.com>
2025-01-11 11:07:37 -0600

    fix bluetooth adapter being turned off

--- .config/waybar/bluetooth_toggle.sh
+++ .config/waybar/bluetooth_toggle.sh
@@ -3,6 +3,7 @@ if [ $STATE = 'yes' ]; then
     bluetoothctl power off    
     notify-send "Bluetooth Off"
 else
+    rfkill unblock bluetooth
     bluetoothctl power on
     notify-send "Bluetooth On"
 fi
