10848d5c79b9512fd5c4d97ca3fac1c6de7f14cf
Bob Varioa <true.email001@gmail.com>
2026-06-06 08:28:26 -0500

    remove notification on toggling bluetooth

--- .config/waybar/bluetooth_toggle.sh
+++ .config/waybar/bluetooth_toggle.sh
@@ -1,9 +1,7 @@
 STATE=`bluetoothctl show | grep Powered | awk '{print $2}'`
 if [ $STATE = 'yes' ]; then    
     bluetoothctl power off    
-    notify-send "Bluetooth Off"
 else
     rfkill unblock bluetooth
     bluetoothctl power on
-    notify-send "Bluetooth On"
 fi
