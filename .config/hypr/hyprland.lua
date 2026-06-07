89a94f405971680a4c3184df226d31fdace8eba4
Bob Varioa <true.email001@gmail.com>
2026-06-07 10:26:52 -0500

    convert float popups to lua

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -87,9 +87,6 @@ hl.on("hyprland.start", function()
         open_glpaper(m)
     end
 
-    -- floating popups --
-    hl.exec_cmd("~/.config/hypr/float-popup.sh")
-
     -- cursor --
     hl.exec_cmd("hyprctl setcursor hyprcursor_Dracula 24")
     hl.exec_cmd("xsetroot -cursor_name left_ptr") -- this is needed to make xcursor work
@@ -104,6 +101,62 @@ end)
 
 hl.on("monitor.removed", function(m)
     close_glpaper(m)
+end)
+
+-- floating popups --
+
+---@param win HL.Window
+---@param matchTag string
+local function hasTag(win, matchTag)
+    local matchedTag = false
+    local tags = win.tags
+    if type(tags) == "string" then
+        for tag in tags:gmatch('[^ ]+') do
+            if tag == matchTag then
+                matchedTag = true
+                break
+            end
+        end
+    elseif type(tags) == "table" then
+        for _, tag in ipairs(tags) do
+            if tag == matchTag then
+                matchedTag = true
+                break
+            end
+        end
+    end
+    return matchedTag
+end
+
+local popupTarget = { W = 500, H = 500, X = 0, Y = 0 }
+
+local function resizePopup(win)
+    hl.timer(function()
+        hl.dispatch(hl.dsp.window.resize({ window = win, x = popupTarget.W, y = popupTarget.H }))
+        hl.dispatch(hl.dsp.window.move({ window = win, x = popupTarget.X, y = popupTarget.Y }))
+    end, { timeout = 50, type = "oneshot" }):set_enabled(true)
+end
+
+---@param win HL.Window
+hl.on("window.active", function(win)
+    if hasTag(win, "float-popup*") then
+        resizePopup(win)
+    else
+        if win.size == nil or win.at == nil then
+            return
+        end
+
+        popupTarget.W = win.size.x / 2
+        popupTarget.H = win.size.y / 2
+        popupTarget.X = win.at.x + popupTarget.W / 2
+        popupTarget.Y = win.at.y + popupTarget.H / 2
+    end
+end)
+
+hl.on("window.open", function(win)
+    if hasTag(win, "float-popup*") then
+        resizePopup(win)
+    end
 end)
 
 hl.config({
